8710fc23388b01ee4602e9cc3a0c628c50ca009f
Pradum <pradumrathour944@gmail.com>
2026-01-27 11:24:57 +0530

    Js basics  practice

--- web-2/script.js
+++ web-2/script.js
@@ -1,35 +1,67 @@
-// let score = prompt("Enter score: ");
-// if (score >= 80 && score <= 100) {
-//     console.log("Grade 'A'");
+// let num = prompt("Enter the number : ")
+// let sum = 0
+// for (let i = 1; i <= num; i++) {
+//     sum += i
 // }
-// else if ( score >= 70 && score < 80 ) {
-//     console.log("Grade 'B'");
+// console.log(sum)
+
+// let string = "Java script"
+// let size = 0
+// for( let i of string){
+//     console.log("i = ",i)
+//     size += 1
 // }
-// else if ( score >= 60 && score < 70 ) {
-//     console.log("Grade 'C'");
+// console.log("size of string is : ",size)
+
+// let gameNumber = 25;
+// let x = prompt("Guess the game number : ")
+// while(x != gameNumber){
+//     let x = prompt("wrong number. Guess again : ")
 // }
-// else if ( score >= 50 && score < 60 ) {
-//     console.log("Grade 'D'");
+// console.log("You Guess the correct number : ", x)
+
+// let str = "Pradumn Rathour"
+// console.log("Length of string is : ",str.length, str[12])
+
+// let Obj = {
+//     name : "Pradum",
+//     cgpa : 6.4
+// };
+// let output = `the cgpa of ${Obj.name} is ${Obj.cgpa}`
+// console.log(output)
+// console.log("The cgpa of ", Obj.name, "is ", Obj.cgpa)
+
+// let str = "pradumn"
+// let username = "@" + str + str.length
+// console.log(username)
+
+// let newStr = str.toLowerCase()
+// console.log(str)
+// console.log(newStr)
+
+// let names = ["pradum", "pratiksha", "piyush", "admino", "Jonny"]
+// for(let i of names){
+//     console.log(i.toUpperCase())
 // }
-// else if ( score < 50 && score >= 0 ) {
-//     console.log("Fail");
+// for (let i = 0; i< names.length; i++){
+//     console.log(names[i].toUpperCase())
 // }
-// else{
-//     console.log("Invalid Score");
+
+// let marks = [39, 98, 87, 73, 44, 67, 18, 73, 45]
+// let sum = 0;
+// console.log(marks.length)
+// for(let i = 0; i < marks.length; i++){
+//     sum += marks[i]
 // }
-// let string = prompt("Enter a string: ");
-// let size = 0;
-// for (let i of string ){
-//     console.log(i);
-//     size += 1;
-// }
-console.log("size of string is: " , size);
-let students = {
-    name: "pradum rathour",
-    age : 21,
-    city : "lucknow",
-    college : "BBDITM",
+// console.log((sum/marks.length).toFixed(2))
+
+let price = [250, 645, 300, 900, 50]
+console.log("Current Price : " + price)
+
+let newPrice = [price.length]
+
+for(let i=0; i<price.length; i++){
+    let x = price[i] - (price[i]/10)
+    newPrice[i] = x
 }
-for (let key in students){
-    console.log(key, ":", students[key] );
-}
+console.log("new price : " + newPrice)
